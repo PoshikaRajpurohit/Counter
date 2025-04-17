e369199561ef07b7d5f79c659880fc734b933fd2
Poshika Rajpurohit <7466poshikarajpurohit@gmail.com>
2025-04-17 11:45:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Counter from './Components/Counter'
+
+
+
+function App() {
+
+  return (
+    <div className="profile">
+      <Counter/>
+    </div>
+  )
+}
+export default App
+
---
+++ src/Components/counter.jsx
@@ -0,0 +1,31 @@
+import { useState}  from "react";
+const Counter =() =>{
+    const[count,setCount]=useState(0);
+    const incre =()=>{
+      setCount(count+1);      
+    }
+    const decre =()=>{
+      setCount(count-1); 
+      if(count<=0) {
+        alert("Counter can't be negative..")
+        setCount(0);
+      }    
+    }
+    const reset =()=>{
+      setCount(0);      
+    }
+    return(
+      <div className="counter-container">
+        <h1>Counter</h1>
+        <h1 className="count">{count}</h1>
+        <div className="btn-group">
+          <button onClick={incre} className="in">Increment</button>
+          <button onClick={decre} className="de">Decrement</button>
+          <button onClick={reset}>Reset</button>
+        </div>
+      </div>
+    )
+}
+
+
+export default Counter;
